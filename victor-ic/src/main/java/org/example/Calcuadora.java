101a57afa8c06f7f6df9b39982207e3c69465298
vls <lermasanchezvictor@gmail.com>
2026-04-09 20:21:02 +0100

    Bug arreglado

--- victor-ic/src/main/java/org/example/Calcuadora.java
+++ victor-ic/src/main/java/org/example/Calcuadora.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Calcuadora {
     public static int sumar(int a, int b) {
-        return a-b;
+        return a+b;
     }
 
     public static int restar(int a, int b) {
